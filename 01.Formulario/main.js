813e3be9db10a0c885b6de71324c59c9677de7b2
supremotix <dennisquispe72@gmail.com>
2021-04-15 20:56:05 -0500

    Primero Commit

---
+++ 01.Formulario/main.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            user: {
+                tittle: "Hola Mundo",
+                name: "Virsis",
+                age: 28,
+                movies: ["Batman","Spiderman","Superman"],
+                url: "https://google.com",
+                image:"https://cdn.pixabay.com/photo/2021/01/22/20/16/heron-5941013_960_720.jpg",
+            },
+        };
+    },
+});
+
